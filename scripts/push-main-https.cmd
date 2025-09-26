8f1256c173f09a451befee85523799563d1b7766
SCHOENFELDER <tschoenfelder@gmx.de>
2025-09-26 16:48:29 +0200

    %~1

--- scripts/push-main-https.cmd
+++ scripts/push-main-https.cmd
@@ -1,34 +1,15 @@
-@echo on
-setlocal ENABLEDELAYEDEXPANSION
-if not exist ".git" (echo ERROR: Bitte im Repo-Root ausfuehren (hier fehlt .git).& exit /b 1)
-for %%%%I in ("%%~dp0") do set "SCRIPTDIR=%%%%~fI"
-set "GIT_ASKPASS=%%SCRIPTDIR%%git-askpass.bat"
-set "SSH_ASKPASS=%%GIT_ASKPASS%%"
+@echo off
+setlocal enabledelayedexpansion
+if not exist ".git" echo ERROR: Bitte im Repo-Root ausfuehren (hier fehlt .git).& exit /b 1
+set "GIT_ASKPASS=%%~dp0git-askpass.bat"
+set "SSH_ASKPASS=%C:\Users\U070420\OneDrive - Lufthansa Group\Local Documents\SmartTScope\scripts\git-askpass.bat%"
 set GIT_TERMINAL_PROMPT=0
-set "TOKENFILE=%C:\Users\U070420\AppData\Local%\SmartTScope\gh_pat.txt"
-if not exist "%%TOKENFILE%%" (echo ERROR: Token-Datei fehlt: %%TOKENFILE%%& exit /b 1)
-for %%%%A in ("%%TOKENFILE%%") do set TOKSIZE=%%%%~zA
-if not defined TOKSIZE set TOKSIZE=0
-if %%TOKSIZE%% LSS 30 (echo ERROR: Token zu kurz (%%TOKSIZE%% Bytes).& exit /b 1)
-git remote set-url origin https://github.com/tschoenfelder/SmartTScope.git
-git config --local credential.helper ""
-if not "%%~1"=="" (
-  git add -A || goto :err
-  git -c core.editor=true commit -m "%%~1" || goto :err
-)
+git config --local credential.helper "" 1>nul 2>nul
+if not "%%~1"=="" git add -A && git -c core.editor=true commit -m "%%~1"
 for /f "delims=" %%%%B in ('git rev-parse --abbrev-ref HEAD') do set CUR=%%%%B
-git fetch origin || goto :err
-set "FLAG=%%~2"
-if /I "%%FLAG%%"=="-M" if /I not "%%CUR%%"=="main" (
-  git switch main || goto :err
-  git pull --ff-only origin main || goto :err
-  git merge --ff-only "%%CUR%%" || goto :err
-)
-for /f "delims=" %%%%B in ('git rev-parse --abbrev-ref HEAD') do set CUR=%%%%B
-if /I not "%%CUR%%"=="main" git switch main || goto :err
-git push -u origin main || goto :err
+if /i not "%%CUR%%"=="main" git switch main || exit /b 1
+git fetch origin || exit /b 1
+git pull --ff-only origin main || exit /b 1
+git push -u origin main || exit /b 1
 echo OK: Push nach origin/main
 exit /b 0
-:err
-echo ERROR: Befehl fehlgeschlagen (Code %0%)
-exit /b 1
